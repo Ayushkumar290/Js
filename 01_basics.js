7817a1e8469bf17e647ca045fd04981b2122beb7
Ayushkumar290 <ayush33kumar3@gmail.com>
2025-01-15 13:40:26 +0530

    Learning Js Basics

---
+++ 01_basics.js
@@ -0,0 +1,10 @@
+console.log("web dev")
+const accountId = 14567
+let accountEmail = "raj1234@gamil.com"
+
+var accountname = "Raj"  // if a variable not defined than it will take as undefined
+/*                                                          
+Note avoid using var 
+*/
+let accountplace ;
+console.table([accountEmail,accountId,accountname,accountplace])                                         
