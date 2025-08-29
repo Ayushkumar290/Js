1a9fe09005aefbd03e87d4b42b6b5163f066b45e
Ayush Kumar <143092664+Ayushkumar290@users.noreply.github.com>
2025-08-29 11:13:37 +0530

    Datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"user strict"; // treat all JS code new  version
+
+
+// alert(2+3) using node JS not browser
+
+console.log(3+1);
+
+console.log("rajesh")
+
+let name = "raj" // string
+let age = 15   // number 2 to the power 53
+let islogged = false //boolean  states true false
+
+// null is a standalone value
+// undefined the veriable is declared but not initalized 
+
+//symbol => unique
+
+//object
+
+console.log(typeof age) //
+console.log(typeof null)// type => object
+console.log(typeof undefined) // type => undefined
